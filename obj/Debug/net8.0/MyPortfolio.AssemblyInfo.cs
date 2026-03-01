f5b050f66de4233c5afb248a999146d3e7221f54
Lousieboyy <mohamadhaikal1551@gmail.com>
2026-03-01 23:35:51 +0800

    Button Scroll to Top

--- obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763a7fb59b7f72b0548499763d718c2e927ce1b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c2eeb5494684f779807a53874df3ab9039b1be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let btnScrollToTop = document.getElementById("btnScrollToTop");
+
+window.onscroll = function () {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        btnScrollToTop.style.display = "block";
+    } else {
+        btnScrollToTop.style.display = "none";
+    }
+}
+btnScrollToTop.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
